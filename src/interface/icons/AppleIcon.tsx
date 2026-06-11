a561a802d916893812946643e03bed9cf6659bd5
natew <natewienert@gmail.com>
2026-06-10 15:52:38 -1000

    fix(web): use named Svg import from react-native-svg to fix dev-mode cjs interop

--- src/interface/icons/AppleIcon.tsx
+++ src/interface/icons/AppleIcon.tsx
@@ -1,4 +1,4 @@
-import Svg, { Path } from 'react-native-svg'
+import { Svg, Path } from 'react-native-svg'
 import { useTheme } from 'tamagui'
 
 import { getIconSize } from '~/interface/icons/helpers'
